65dcff46b17808be74089510e7f3dbae026b7d9d
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2023-07-14 17:19:50 +0200

    pre-commit and pre-push added

--- lib/git_policy/pre_push.py
+++ lib/git_policy/pre_push.py
@@ -2,19 +2,61 @@
 
 import sys
 import subprocess
+from decouple import config
+
+git_executable = ["git"]
+
+SIG_COMMIT = config("SIG_COMMIT")
+ALLOWED_SIGNERS_FILE = config("ALLOWED_SIGNERS_FILE")
+ACTIVE_EMAIL = config("ACTIVE_EMAIL")
+ACTIVE_SIGNING_KEY = config("ACTIVE_SIGNING_KEY")
+
+try:
+    subprocess.check_output(["git", "config", "--get", "commit.gpgsign"])
+except subprocess.CalledProcessError as e:
+    if SIG_COMMIT:
+        git_executable += [f"--config-env=commit.gpgsign=SIG_COMMIT"]
+
+try:
+    subprocess.check_output(["git", "config", "--get", "gpg.ssh.allowedSignersFile"])
+except subprocess.CalledProcessError as e:
+    if ALLOWED_SIGNERS_FILE:
+        git_executable += [
+            f"--config-env=gpg.ssh.allowedSignersFile=ALLOWED_SIGNERS_FILE"
+        ]
+
+try:
+    subprocess.check_output(["git", "config", "--get", "user.email"])
+except subprocess.CalledProcessError as e:
+    if ACTIVE_EMAIL:
+        git_executable += [f"--config-env=user.email=ACTIVE_EMAIL"]
+
+try:
+    subprocess.check_output(["git", "config", "--get", "user.signingKey"])
+except subprocess.CalledProcessError as e:
+    if ACTIVE_SIGNING_KEY:
+        git_executable += [f"--config-env=user.signingKey=ACTIVE_SIGNING_KEY"]
 
 if __name__ == "__main__":
     for line in sys.stdin:
         local_ref, local_sha1, remote_ref, remote_sha1 = line.strip().split()
-        message = subprocess.check_output(
-            ['git', 'log', '--format=format:%H', local_sha1, f"^{remote_sha1}"])
+        if remote_sha1.startswith("000000000000000"):
+            sys.exit(0)
+        cmd = git_executable + [
+            "log",
+            "--format=format:%H",
+            local_sha1,
+            f"^{remote_sha1}",
+        ]
+        message = subprocess.check_output(cmd)
         for sha in message.decode("UTF-8").split("\n"):
             try:
-                res = subprocess.check_output(
-                    ['git', 'verify-commit', '-v', sha])
+                cmd = git_executable + ["verify-commit", "-v", sha]
+                res = subprocess.check_output(cmd)
             except subprocess.CalledProcessError as e:
                 print(e)
                 sys.exit(1)
+
 
 def put_as_pre_push_githook():
     import shutil
