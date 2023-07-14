7af97b13ccf52851f3fb770422729aa8ddd78ee9
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2023-07-14 16:28:37 +0200

    pre-commit and pre-push added

--- lib/git_policy/pre_commit.py
+++ lib/git_policy/pre_commit.py
@@ -5,20 +5,21 @@ import re
 from git import Repo
 from decouple import config
 
-VERIFIER_TYPES = config('REPO_VERIFIER_TYPES', default='(feat|fix|bugfix|config|refactor|build|ci|docs|test)')
-VERIFIER_TASKS_KEYS = config('REPO_VERIFIER_TASKS_KEYS', default='ZDLY-[0-9]+')
+if __name__ == "__main__":
+    VERIFIER_TYPES = config('REPO_VERIFIER_TYPES', default='(feat|fix|bugfix|config|refactor|build|ci|docs|test)')
+    VERIFIER_TASKS_KEYS = config('REPO_VERIFIER_TASKS_KEYS', default='ZDLY-[0-9]+')
 
-repo = Repo(".")
+    repo = Repo(".")
 
-types=VERIFIER_TYPES
-task_management_key=VERIFIER_TASKS_KEYS
-description="[A-Za-z0-9\\-]+"
+    types=VERIFIER_TYPES
+    task_management_key=VERIFIER_TASKS_KEYS
+    description="[A-Za-z0-9\\-]+"
 
-pattern = f"^{types}/{task_management_key}{description}$"
+    pattern = f"^{types}/{task_management_key}{description}$"
 
-if not re.search(pattern, str(repo.active_branch)):
-    print(f"Active branch name is not valid, please follow the pattern:\n {pattern}")
-    sys.exit(1)
+    if not re.search(pattern, str(repo.active_branch)):
+        print(f"Active branch name is not valid, please follow the pattern:\n {pattern}")
+        sys.exit(1)
 
 def put_as_pre_commit_githook():
     import shutil
--- lib/git_policy/pre_push.py
+++ lib/git_policy/pre_push.py
@@ -3,17 +3,18 @@
 import sys
 import subprocess
 
-for line in sys.stdin:
-    local_ref, local_sha1, remote_ref, remote_sha1 = line.strip().split()
-    message = subprocess.check_output(
-        ['git', 'log', '--format=format:%H', local_sha1, f"^{remote_sha1}"])
-    for sha in message.decode("UTF-8").split("\n"):
-        try:
-            res = subprocess.check_output(
-                ['git', 'verify-commit', '-v', sha])
-        except subprocess.CalledProcessError as e:
-            print(e)
-            sys.exit(1)
+if __name__ == "__main__":
+    for line in sys.stdin:
+        local_ref, local_sha1, remote_ref, remote_sha1 = line.strip().split()
+        message = subprocess.check_output(
+            ['git', 'log', '--format=format:%H', local_sha1, f"^{remote_sha1}"])
+        for sha in message.decode("UTF-8").split("\n"):
+            try:
+                res = subprocess.check_output(
+                    ['git', 'verify-commit', '-v', sha])
+            except subprocess.CalledProcessError as e:
+                print(e)
+                sys.exit(1)
 
 def put_as_pre_push_githook():
     import shutil
